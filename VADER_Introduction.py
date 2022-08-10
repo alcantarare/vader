2f5e9eb47d8054ba674775217734b1a43572dce6
alcantarare <50539422+alcantarare@users.noreply.github.com>
2022-08-10 14:16:53 +0800

    Add files via upload

---
+++ VADER_Introduction.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Introduction",
+    page_icon="📖"
+)
+
+st.write("# Welcome To My Final Project Dashboard")
+
+st.markdown("""
+## VADER-Sentiment-Analysis
+VADER (Valence Aware Dictionary and sEntiment Reasoner) is a lexicon and rule-based sentiment analysis tool that is specifically attuned to sentiments expressed in social media.
+""")
+
+st.write("""
+### Created By:
+C.J. Hutto and Eric Gilbert
+VADER: A Parsimonious Rule-based Model for Sentiment Analysis of Social Media Text
+Eighth International Conference on Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.
+""")
