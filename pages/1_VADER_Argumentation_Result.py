e8b365f61001a81c404599f93b87636e1a3ff709
Husna Rezal Dewantara <50539422+rezaldwntr@users.noreply.github.com>
2022-09-01 11:15:20 +0800

    Update 1_VADER_Argumentation_Result.py

--- pages/1_VADER_Argumentation_Result.py
+++ pages/1_VADER_Argumentation_Result.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 #from sklearn.metrics import classification_report
 #cr = classification_report
-import matplotlib.pyplot as plts
+#import matplotlib.pyplot as plts
 
 sid = SentimentIntensityAnalyzer()
 
